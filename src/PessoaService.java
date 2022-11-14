2636f9e92ab4a9796b5c8563880f9bf9eb5eafdb
Lara Raffel <lara.b.r@outlook.com>
2022-11-14 18:12:56 -0300

    apostila01(oracle_cloud): bloco de codigo 2.10.1. exibindo apenas nome, sem interacao sobre a colecao

--- src/PessoaService.java
+++ src/PessoaService.java
@@ -1,23 +1,27 @@
-import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import lombok.RequiredArgsConstructor;
+import java.net.URI;
 
-@RequiredArgsConstructor
+@RequiredArgsConstructor// gerar construtor para inicializar variaveis
 public class PessoaService {
-    private static HttpClient client = HttpClient.newHttpClient();
-    private final String url;
-
-
+    private static HttpClient client = HttpClient.newHttpClient();//cliente http, variavel de referencia
+    private final String url;//constante
     public void listar(){
         try{
             //design pattern: builder
-            HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create(url)).
-            build();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();// manipulador de corpo de resposta com o conteúdo de uma String
             var response = client.send(request, BodyHandlers.ofString());
             System.out.println(response.body());
+            JSONObject raiz = new JSONObject(response.body());
+            JSONArray items = raiz.getJSONArray("items");
+            JSONObject primeiro = items.getJSONObject(0);
+            //System.out.println(primeiro);
+            String nome = primeiro.getString("nome");
+            System.out.println(nome);
         }
 
         catch(Exception e){
